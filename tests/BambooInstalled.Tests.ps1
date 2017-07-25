f41653693183df941e68cdaa5abef165696c97e9
Chris O'Dell <chris.odell@skeltonthatcher.com>
2017-07-25 17:37:00 +0100

    Adds test for firewall rule
    Also amends test descriptions to match Pester patterns

--- tests/BambooInstalled.Tests.ps1
+++ tests/BambooInstalled.Tests.ps1
@@ -1,13 +1,17 @@
-Describe  'Bamboo Server installed' {
-    It 'Installation directory exists' {
+Describe  'Bamboo Server installation' {
+    It 'creates the installation directory' {
         Test-Path "C:\Program Files\Bamboo" | Should be $true
     }
 
-    It 'Home directory exists' {
+    It 'creates the Home directory' {
         Test-Path "C:\Users\Administrator\Bamboo-home" | Should be $true
     }
 
-    It 'Service exists' {
+    It 'sets up the Service' {
         Get-Service "bamboo" | Should be $true
+    }
+
+    It 'creates the inbound firewall rule' {
+        Get-NetFirewallRule -Name "Bamboo Server" | Should Not BeNullOrEmpty
     }
 }
