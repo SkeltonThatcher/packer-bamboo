64f8a0571728120e32839ec7571ff74dba5d2d08
Chris O'Dell <chris.odell@skeltonthatcher.com>
2017-07-25 16:56:39 +0100

    Run the installation in process so that we wait for completion

--- scripts/InstallBambooServer.ps1
+++ scripts/InstallBambooServer.ps1
@@ -2,10 +2,10 @@
 Invoke-WebRequest -URI "http://www.atlassian.com/software/bamboo/downloads/binary/atlassian-bamboo-6.1.0-windows-x64.exe" -OutFile "C:\Windows\Temp\atlassian-bamboo-6.1.0-windows-x64.exe"
 
 # Silently install Bamboo
-C:\Windows\Temp\atlassian-bamboo-6.1.0-windows-x64.exe -q
+Start-Process C:\Windows\Temp\atlassian-bamboo-6.1.0-windows-x64.exe -ArgumentList "-q" -Wait
 
 # Install as a service
-& "C:\Program Files\Bamboo\installasservice.bat"
+& "C:\Program Files\Bamboo\InstallAsService.bat"
 
 # Set Creds to local Admin
 # Figure out how to get the password
