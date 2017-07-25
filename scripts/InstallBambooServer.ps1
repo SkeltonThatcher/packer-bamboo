bce16313dc93496418aa6cd1863f35949e05776c
Chris O'Dell <chris.odell@skeltonthatcher.com>
2017-07-25 16:57:36 +0100

    Adds outputting of log messages

--- scripts/InstallBambooServer.ps1
+++ scripts/InstallBambooServer.ps1
@@ -1,17 +1,17 @@
-# Download Bamboo
+Write-Host "Downloading Bamboo"
 Invoke-WebRequest -URI "http://www.atlassian.com/software/bamboo/downloads/binary/atlassian-bamboo-6.1.0-windows-x64.exe" -OutFile "C:\Windows\Temp\atlassian-bamboo-6.1.0-windows-x64.exe"
 
-# Silently install Bamboo
+Write-Host "Installing Bamboo"
 Start-Process C:\Windows\Temp\atlassian-bamboo-6.1.0-windows-x64.exe -ArgumentList "-q" -Wait
 
-# Install as a service
+Write-Host "Installing Bamboo as a Service"
 & "C:\Program Files\Bamboo\InstallAsService.bat"
 
 # Set Creds to local Admin
 # Figure out how to get the password
 # https://blog.ipswitch.com/working-with-windows-services-in-powershell
 
-# Set to automatic run & start it
+Write-Host "Setting Bamboo Service to Automatic startup and then starting"
 Set-Service Bamboo -startuptype "Automatic"
 Start-Service Bamboo
 
