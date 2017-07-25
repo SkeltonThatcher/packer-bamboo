0d2cd0468c2b2b2ddf485bbc1116a8822698a69d
Chris O'Dell <chris.odell@skeltonthatcher.com>
2017-07-25 12:18:13 +0100

    Runs the service setup directly so that error information is displayed. Also run the Bamboo service.

--- scripts/InstallBambooServer.ps1
+++ scripts/InstallBambooServer.ps1
@@ -5,11 +5,12 @@ Invoke-WebRequest -URI "http://www.atlassian.com/software/bamboo/downloads/binar
 C:\Windows\Temp\atlassian-bamboo-6.1.0-windows-x64.exe -q
 
 # Install as a service
-Start-Process "cmd.exe" "/c C:\Program Files\Bamboo\install-service.bat"
+& "C:\Program Files\Bamboo\installasservice.bat"
 
 # Set Creds to local Admin
 # Figure out how to get the password
 # https://blog.ipswitch.com/working-with-windows-services-in-powershell
 
-# Set to automatic run
+# Set to automatic run & start it
 Set-Service Bamboo -startuptype "Automatic"
+Start-Service Bamboo
